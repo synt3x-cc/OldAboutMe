f242e5c841db514168f83aed48688932dcbc2e76
synt3x-cc <energyexecutor1@gmail.com>
2025-02-02 18:18:36 -0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Example of smooth scroll on clicking navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
